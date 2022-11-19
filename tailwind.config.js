d9e571df73a9cd3e6e1bf820c77a5d84f7b9a3f2
gurcancekic <arinyazilim@gmail.com>
2022-11-19 15:40:26 +0300

    Lesson 03

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+
+      colors: {
+        'gega-red': '#BC1A45',
+        'gega-melon': '#FFD369',
+        'gega-grey': '#DDDDDD',
+        'gega-white': '#F7F7F7',
+      },
+    },
   },
   plugins: [],
 };
